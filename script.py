27b4b36b81181fbb449eac98122c7d5b3b7eee37
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-10 17:34:25 -0500

    added logic testing os.listdir() function, works just fine (within script.py)

--- script.py
+++ script.py
@@ -1,9 +1,20 @@
 ############------------ IMPORTS ------------############
+import os
+
 
 ############------------ GLOBAL VARIABLES ------------############
 
 ############------------ FUNCTIONS ------------############
+def rename_files():
+    '''
+     reads files in a directory, and
+     renames them.
+    '''
+    # testing os.listdir() function
+    for image in os.listdir('images'):
+        print(image)
+
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    pass
+    rename_files()
