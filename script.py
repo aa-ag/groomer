f003e02f6e847cb4816476f7be124fb7f0bccc09
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-10 18:19:28 -0500

    finalized logic to renamed all files in directory, was missing source and destination path as well as os.path.join() to let program know to get that information while executing

--- script.py
+++ script.py
@@ -10,10 +10,20 @@ def rename_files():
      reads files in a directory, and
      renames them.
     '''
-    # testing os.listdir() function
-    for i, image in enumerate(os.listdir('images')):
-        file_name, file_extension = os.path.splitext(image)
-        print(i, file_name, file_extension)
+    # set source and destination of images
+    # where to go get the images from/put them in
+    source_path = 'images'
+    destination_path = 'images'
+
+    # create a list of images to iterate over
+    images = os.listdir('images')
+
+    # use enumerate to add count/n to new title
+    for i, image in enumerate(images):
+        existing_name, file_extension = image.split('.')
+        new_name = f"test_{i}.{file_extension}"
+        os.rename(os.path.join(source_path, image),
+                  os.path.join(destination_path, new_name))
 
 
 ############------------ DRIVER CODE ------------############
