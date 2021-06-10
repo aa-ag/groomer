37b426b4b629ca1f528403cf3772e54fa6f7a14d
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-10 18:33:23 -0500

    added logic to zip() test_brands and images (or their file names anyway) to combine the two and thus create each new file name

--- script.py
+++ script.py
@@ -3,6 +3,8 @@ import os
 
 
 ############------------ GLOBAL VARIABLES ------------############
+test_brands = ['i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii', 'viii', 'ix']
+
 
 ############------------ FUNCTIONS ------------############
 def rename_files():
@@ -10,6 +12,9 @@ def rename_files():
      reads files in a directory, and
      renames them.
     '''
+    # import test_brands list
+    global test_brands
+
     # set source and destination of images
     # where to go get the images from/put them in
     source_path = 'images'
@@ -18,12 +23,17 @@ def rename_files():
     # create a list of images to iterate over
     images = os.listdir('images')
 
+    # zip images & brands to combine the two
+    # in creating new file names
+    for brand, image_name in zip(test_brands, images):
+        print(brand, image_name)
+
     # use enumerate to add count/n to new title
-    for i, image in enumerate(images):
-        existing_name, file_extension = image.split('.')
-        new_name = f"test_{i}.{file_extension}"
-        os.rename(os.path.join(source_path, image),
-                  os.path.join(destination_path, new_name))
+    # for i, image_name in enumerate(images):
+    #     existing_name, file_extension = image_name.split('.')
+    #     new_name = f"{i}_header.png"
+    #     os.rename(os.path.join(source_path, image_name),
+    #               os.path.join(destination_path, new_name))
 
 
 ############------------ DRIVER CODE ------------############
